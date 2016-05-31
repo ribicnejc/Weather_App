d060845519d500fbe5b1b17678901fa42359f3d6
ribicnejc <nejc.ribic95@gmail.com>
2016-05-31 23:01:19 +0200

    Updating

--- app/src/main/java/com/povio/weatherapp/MoreInfoActivity.java
+++ app/src/main/java/com/povio/weatherapp/MoreInfoActivity.java
@@ -2,8 +2,8 @@ package com.povio.weatherapp;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,11 +12,13 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.romainpiel.shimmer.ShimmerButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MoreInfoActivity extends AppCompatActivity {
 
-    Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,52 +51,51 @@ public class MoreInfoActivity extends AppCompatActivity {
     public void onFinish(GetWeatherInfoAPI api, ForeCastAPI foreCastAPI) {
         setContentView(R.layout.activity_weather_info_update);
         TextView cityName;
-        TextView sunset;
-        TextView sunrise;
-        TextView minTemp;
-        TextView maxTemp;
-        TextView temp;
-        TextView humidity;
-        TextView pressure;
         TextView weatherDesc;
         TextView weatherInfo;
-        TextView realFeel;
         ImageView weatherPhoto;
-        ShimmerButton button;
         ImageView firstIcon;
         TextView firstTime;
         TextView firstTemp;
 
+        TextView[] days = new TextView[5];
+        TextView[] daysTempMin = new TextView[5];
+        TextView[] daysTempMax = new TextView[5];
+        ImageView[] daysIcons = new ImageView[5];
+
         try{
-        cityName = (TextView) findViewById(R.id.cityMoreInfo);
-        cityName.setText(api.getCityName() + ", " + api.getCountry());
-        weatherInfo = (TextView) findViewById(R.id.infoMoreInfo);
-        weatherInfo.setText(api.getMainDesc());
-        weatherDesc = (TextView) findViewById(R.id.descMoreInfo);
-        weatherDesc.setText(api.getIconDesc());
-        weatherPhoto = (ImageView) findViewById(R.id.iconMoreInfo);
-        weatherPhoto.setImageResource(api.getIcon());
-//        temp = (TextView) findViewById(R.id.tempMoreInfo);
-//        temp.setText(api.getMainTemp());
-//        minTemp = (TextView) findViewById(R.id.minTempMoreInfo);
-//        minTemp.setText(api.getMinTemp() + "°C");
-//        maxTemp = (TextView) findViewById(R.id.maxTempMoreInfo);
-//        maxTemp.setText(api.getMaxTemp() + "°C");
-//        humidity = (TextView) findViewById(R.id.humidityMoreInfo);
-//        humidity.setText(api.getHumidity() + "%");
-//        pressure = (TextView) findViewById(R.id.pressureMoreInfo);
-//        pressure.setText(api.getPressure() + " hPa");
-//        realFeel = (TextView) findViewById(R.id.realFeelMoreInfo);
-//        realFeel.setText("Real feel: " + api.getRealFeel() + "°C");
-
-        firstTime = (TextView) findViewById(R.id.firstTime);
-        firstTime.setText(foreCastAPI.getTimeL().get(0));
-        firstIcon = (ImageView) findViewById(R.id.firstIcon);
-        firstIcon.setImageResource(foreCastAPI.getIconL().get(0));
-        firstTemp = (TextView) findViewById(R.id.firstTemp);
-        firstTemp.setText(foreCastAPI.getMainTempL().get(0));
-
-
+            cityName = (TextView) findViewById(R.id.cityMoreInfo);
+            cityName.setText(api.getCityName() + ", " + api.getCountry());
+            weatherInfo = (TextView) findViewById(R.id.infoMoreInfo);
+            weatherInfo.setText(api.getMainDesc());
+            weatherDesc = (TextView) findViewById(R.id.descMoreInfo);
+            weatherDesc.setText(api.getIconDesc());
+            weatherPhoto = (ImageView) findViewById(R.id.iconMoreInfo);
+            weatherPhoto.setImageResource(api.getIcon());
+            firstTime = (TextView) findViewById(R.id.firstTime1);
+            firstTime.setText(foreCastAPI.getTimeL().get(0));
+            firstIcon = (ImageView) findViewById(R.id.firstIcon1);
+            firstIcon.setImageResource(foreCastAPI.getIconL().get(0));
+            firstTemp = (TextView) findViewById(R.id.firstTemp1);
+            firstTemp.setText(foreCastAPI.getMainTempL().get(0));
+            int i = 0;
+            int j = 0;
+            for (String elt : foreCastAPI.getTimeL()){
+                if (elt.equals("12:00")){
+                    int resId = getResources().getIdentifier("day" + (i+1) + "_icon", "id", getPackageName());
+                    daysIcons[i] = (ImageView) findViewById(resId);
+                    daysIcons[i].setImageResource(foreCastAPI.getIconL().get(j));
+                    SimpleDateFormat newDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.ENGLISH);
+                    Date MyDate = newDateFormat.parse(foreCastAPI.getDateAndTimeL().get(j));
+                    newDateFormat.applyPattern("EEEE");
+                    String nameOfDay = newDateFormat.format(MyDate);
+                    int dayResId = getResources().getIdentifier("day" + (i+1), "id", getPackageName());
+                    days[i] = (TextView) findViewById(dayResId);
+                    days[i].setText(nameOfDay);
+                    i++;
+                }
+                j++;
+            }
         }catch (Exception e){
             setContentView(R.layout.error);
             Toast.makeText(MoreInfoActivity.this, "No internet connection", Toast.LENGTH_SHORT).show();
