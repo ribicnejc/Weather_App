9404ef74cbc391c14e3a57649342d3834252ef33
Nejc Ribic <nejc.ribic95@gmail.com>
2016-06-12 22:27:47 +0200

    Widget

---
+++ app/src/main/java/com/povio/weatherapp/Widget/WidgetProvider.java
@@ -0,0 +1,45 @@
+package com.povio.weatherapp.Widget;
+
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.povio.weatherapp.CityQuery;
+import com.povio.weatherapp.GetWeatherInfoAPI;
+import com.povio.weatherapp.R;
+
+import java.util.Random;
+
+
+public class WidgetProvider extends AppWidgetProvider{
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+        final  int nb = appWidgetIds.length;
+        for(int i = 0; i < nb; i++){
+            int widgetId = appWidgetIds[i];
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+            //GetWeatherInfoAPI api = new GetWeatherInfoAPI();
+            //set elements values
+            remoteViews.setTextViewText(R.id.widget_clock, new Random().nextInt(50)+"");
+
+            Intent configIntent = new Intent (context, CityQuery.class);
+            configIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds);
+            PendingIntent pIntent = PendingIntent.getActivity(context, 0, configIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.widget_clock, pIntent);
+
+            Intent intent = new Intent(context, WidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0 , intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.widget_image, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+
+}
