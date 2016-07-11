bd108985ca043829900c22a10c7149d1cd9e20f6
Nejc Ribic <nejc.ribic95@gmail.com>
2016-07-11 17:42:39 +0200

    Updating

--- app/src/main/java/com/povio/weatherapp/Constants.java
+++ app/src/main/java/com/povio/weatherapp/Constants.java
@@ -4,5 +4,8 @@ package com.povio.weatherapp;
  * Created by Nejc on 29. 06. 2016.
  */
 public class Constants {
+    public static boolean TEMPERATURE_BOOL = false;
+    public static boolean TYPE_BOOL = false;
+    public static boolean NAME_BOOL = false;
 
 }
--- app/src/main/java/com/povio/weatherapp/NavigationDrawerFragment.java
+++ app/src/main/java/com/povio/weatherapp/NavigationDrawerFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +14,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class NavigationDrawerFragment extends Fragment {
 
@@ -21,10 +24,14 @@ public class NavigationDrawerFragment extends Fragment {
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
 
+    private SharedPreferences sharedPreferences;
+    private RadioButton radioButtonTemp, radioButtonType, radioButtonName;
+
     private View containerView;
     private boolean mUserLearnedDrawer;
     private boolean mFromSavedInstanceState;
     private LinearLayout linearLayoutHomeButton;
+
     public NavigationDrawerFragment() {
         // Required empty public constructor
     }
@@ -33,7 +40,7 @@ public class NavigationDrawerFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mUserLearnedDrawer = Boolean.valueOf(readFromPreferences(getActivity(), KEY_LEARNED_DRAWER, "false"));
-        if( savedInstanceState != null){
+        if (savedInstanceState != null) {
             mFromSavedInstanceState = true;
         }
 
@@ -44,6 +51,18 @@ public class NavigationDrawerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+
+
+        radioButtonTemp = (RadioButton) view.findViewById(R.id.navigation_drawer_radio_temp);
+        radioButtonType = (RadioButton) view.findViewById(R.id.navigation_drawer_radio_type);
+        radioButtonName = (RadioButton) view.findViewById(R.id.navigation_drawer_radio_name);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        if(sharedPreferences.contains("radioButtonName")) {
+            radioButtonName.setChecked(sharedPreferences.getBoolean("radioButtonName", false));
+            radioButtonTemp.setChecked(sharedPreferences.getBoolean("radioButtonTemp", false));
+            radioButtonType.setChecked(sharedPreferences.getBoolean("radioButtonType", false));
+        }
         linearLayoutHomeButton = (LinearLayout) view.findViewById(R.id.navigation_drawer_home_button);
         linearLayoutHomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,30 +79,44 @@ public class NavigationDrawerFragment extends Fragment {
                 intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
                 startActivity(intent);
                 (getActivity()).overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("radioButtonTemp", radioButtonTemp.isChecked());
+                editor.putBoolean("radioButtonType", radioButtonType.isChecked());
+                editor.putBoolean("radioButtonName", radioButtonName.isChecked());
+                editor.apply();
             }
         });
         // Inflate the layout for this fragment
         return view;
     }
 
-    public void setUp(int fragmentId , DrawerLayout drawerLayout, final Toolbar toolbar) {
+    public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
         containerView = getActivity().findViewById(fragmentId);
         mDrawerLayout = drawerLayout;
-        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close){
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-                if(!mUserLearnedDrawer){
+                if (!mUserLearnedDrawer) {
                     mUserLearnedDrawer = true;
                     saveToPreference(getActivity(), KEY_LEARNED_DRAWER, mUserLearnedDrawer + "");
                 }
                 getActivity().invalidateOptionsMenu();
+                radioButtonName.setChecked(sharedPreferences.getBoolean("radioButtonName", false));
+                radioButtonTemp.setChecked(sharedPreferences.getBoolean("radioButtonTemp", false));
+                radioButtonType.setChecked(sharedPreferences.getBoolean("radioButtonType", false));
             }
 
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
                 getActivity().invalidateOptionsMenu();
+                Toast.makeText(getActivity(), "Shranim podatke ?", Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("radioButtonTemp", radioButtonTemp.isChecked());
+                editor.putBoolean("radioButtonType", radioButtonType.isChecked());
+                editor.putBoolean("radioButtonName", radioButtonName.isChecked());
+                editor.apply();
             }
 
             @Override
@@ -95,7 +128,7 @@ public class NavigationDrawerFragment extends Fragment {
         };
 
         mDrawerLayout.addDrawerListener(mDrawerToggle);
-        mDrawerLayout.post(new Runnable(){
+        mDrawerLayout.post(new Runnable() {
             @Override
             public void run() {
                 mDrawerToggle.syncState();
@@ -103,13 +136,14 @@ public class NavigationDrawerFragment extends Fragment {
         });
     }
 
-    public static void saveToPreference(Context context, String prefreneceName, String preferenceValue){
+    public static void saveToPreference(Context context, String prefreneceName, String preferenceValue) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(prefreneceName, preferenceValue);
         editor.apply();
     }
-    public static String readFromPreferences(Context context, String preferenceName, String defaultValue){
+
+    public static String readFromPreferences(Context context, String preferenceName, String defaultValue) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
         return sharedPreferences.getString(preferenceName, defaultValue);
     }
