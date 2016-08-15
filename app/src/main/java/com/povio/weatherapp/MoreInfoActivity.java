d73bfab486312e29561d5fd390b90cce6cd96003
Nejc Ribic <nejc.ribic95@gmail.com>
2016-08-15 12:17:19 +0200

    Search City Activity Fix

--- app/src/main/java/com/povio/weatherapp/MoreInfoActivity.java
+++ app/src/main/java/com/povio/weatherapp/MoreInfoActivity.java
@@ -46,9 +46,9 @@ public class MoreInfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.loading_before_moreinfo);
         ProgressBar pr;
         pr = (ProgressBar) findViewById(R.id.progressBarMoreInfo);
